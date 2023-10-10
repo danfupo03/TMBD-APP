9b3ed5778427148005e13b1fdc6bf25c9540a499
danfupo03 <danfupo@outlook.com>
2023-10-10 11:57:16 -0600

    ...

---
+++ TMBD/TMBD/TMBDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMBDApp.swift
+//  TMBD
+//
+//  Created by Dan FuPo on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TMBDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
