d1795af9fee5e4c903cc26f5fcc25ce2f1f7a9ac
danfupo03 <danfupo@outlook.com>
2023-12-07 17:53:48 -0600

    chore(DetailView): Redisign of the detail view

--- TMBD/Framework/Views/MovieDetailView.swift
+++ TMBD/Framework/Views/MovieDetailView.swift
@@ -23,11 +23,9 @@ struct MovieDetailView: View {
       Rectangle()
         .foregroundColor(Color.black)
         .opacity(0.8)
-        .frame(width: 430, height: 410)
-        .cornerRadius(10)
-        .offset(x: 0, y: 233)
+        .ignoresSafeArea()
       
-      VStack(alignment: .leading, spacing: 10) {
+      VStack {
         HStack {
           WebImage(url: movie.fullPoster)
             .resizable()
@@ -40,26 +38,36 @@ struct MovieDetailView: View {
             .font(Font.custom("Inter", size: 25).weight(.bold))
             .foregroundColor(.white)
         }
+        .frame(width: 350)
         
-      } .offset(x: 0, y: 60)
+        VStack (alignment: .leading) {
+          Text("Overview").font(.title)
+            .foregroundStyle(.white)
+            .font(.largeTitle)
+            .padding([.top, .bottom])
+          
+          Text(movie.overview)
+            .foregroundStyle(.white)
+          
+        } .padding([.trailing, .leading], 70)
+      }
       
     }
-    //.frame(width: 430, height: 932)
   }
 }
 
 #Preview {
-  MovieDetailView(movie: Movie(id: 968051,
-                               genre_ids: [27, 93],
+  MovieDetailView(movie: Movie(id: 901362,
+                               genre_ids: [16, 10751, 10402, 14, 35],
                                adult: false,
                                backdrop_path: "/mRGmNnh6pBAGGp6fMBMwI8iTBUO.jpg",
                                original_language: "en",
                                original_title: "The Nun II",
-                               overview: "In 1956 France, a priest is violently murdered, and Sister Irene begins to investigate. She once again comes face-to-face with a powerful evil.",
+                               overview: "When Branch's brother, Floyd, is kidnapped for his musical talents by a pair of nefarious pop-star villains, Branch and Poppy embark on a harrowing and emotional journey to reunite the other brothers and rescue Floyd from a fate even worse than pop-culture obscurity.",
                                popularity: 4160.929,
-                               poster_path: "/5gzzkR7y3hnY8AD1wXjCnVlHba5.jpg",
+                               poster_path: "/bkpPTZUdq31UGDovmszsg2CchiI.jpg",
                                release_date: "2023-09-06",
-                               title: "The Nun II",
+                               title: "Trolls Band Together",
                                video: false,
                                vote_average: 7,
                                vote_count: 776))
