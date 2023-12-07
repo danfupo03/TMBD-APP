d3217b92b1fde4976800752cf988e85fb4139341
danfupo03 <danfupo@outlook.com>
2023-12-07 17:10:11 -0600

    feat(TabBar): Add TabBar to app

---
+++ TMBD/Framework/Views/Components/TabBar/Home.swift
@@ -0,0 +1,117 @@
+//
+//  Home.swift
+//  TMBD
+//
+//  Created by Dan FuPo on 03/12/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+  @State private var activeTab: Tab = .home
+  
+  @Namespace private var animation
+  @State private var tabShapePosition: CGPoint = .zero
+  init() {
+    UITabBar.appearance().isHidden = true
+  }
+  var body: some View {
+    VStack(spacing: 0) {
+      TabView(selection: $activeTab) {
+        MovieList()
+          .tag(Tab.home)
+          //.toolbar(.hidden, for: .tabBar)
+        
+        Text("Services")
+          .tag(Tab.services)
+          //.toolbar(.hidden, for: .tabBar)
+        
+        Text("Partners")
+          .tag(Tab.partners)
+          //.toolbar(.hidden, for: .tabBar)
+        
+        Text("Activity")
+          .tag(Tab.activity)
+          //.toolbar(.hidden, for: .tabBar)
+      }
+      
+      CustomTabBar()
+    }
+  }
+  
+  @ViewBuilder
+  func CustomTabBar(_ tint: Color = Color("Blue"), _ inactiveTint: Color = .blue) -> some View {
+    HStack(alignment: .bottom, spacing: 0) {
+      ForEach(Tab.allCases, id: \.rawValue) {
+        TabItem(
+          tint: tint,
+          inactiveTint: inactiveTint,
+          tab: $0,
+          animation: animation,
+          activeTab: $activeTab,
+          position: $tabShapePosition)
+      }
+    }
+    .padding(.horizontal, 15)
+    .padding(.vertical, 10)
+    .background(content: {
+      TabShape(midpoint: tabShapePosition.x)
+        .fill(.white)
+        .ignoresSafeArea()
+        .shadow(color: tint.opacity(0.2), radius: 5, x: 0, y: -5)
+        .blur(radius: 2)
+        .padding(.top, 25)
+    })
+    .animation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7), value: activeTab)
+  }
+}
+
+struct TabItem: View {
+  var tint: Color
+  var inactiveTint: Color
+  var tab: Tab
+  var animation: Namespace.ID
+  @Binding var activeTab: Tab
+  @Binding var position: CGPoint
+  
+  @State private var tabPosition: CGPoint = .zero
+  var body: some View {
+    VStack(spacing: 5) {
+      Image(systemName: tab.systemImage)
+        .font(.title2)
+        .foregroundStyle(activeTab == tab ? .white : inactiveTint)
+        .frame(width: activeTab == tab ? 58 : 35, height: activeTab == tab ? 58 : 35)
+        .background{
+          if activeTab == tab {
+            Circle()
+              .fill(tint.gradient)
+              .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+          }
+        }
+      
+      Text(tab.rawValue)
+        .font(.caption)
+        .foregroundStyle(activeTab == tab ? tint : .gray)
+    }
+    .frame(maxWidth: .infinity)
+    .contentShape(Rectangle())
+    .viewPosition(completion: { rect in
+      tabPosition.x = rect.midX
+      
+      if activeTab == tab {
+        position.x = rect.midX
+      }
+    })
+    .onTapGesture {
+      activeTab = tab
+      
+      withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+        position.x = tabPosition.x
+      }
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
--- TMBD/Framework/Views/ContentView.swift
+++ TMBD/Framework/Views/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Home()
-      .preferredColorScheme(.light)
+    MovieList()
+//    Home()
+//      .preferredColorScheme(.light)
   }
 }
 
