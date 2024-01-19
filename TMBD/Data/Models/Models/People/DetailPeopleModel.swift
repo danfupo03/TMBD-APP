a87ebcf1d2b58e780f1820be582c1a4af9ca84a9
danfupo03 <danfupo@outlook.com>
2024-01-19 17:30:39 -0600

    chore(Reorganize): Reorganize files

---
+++ TMBD/Data/Models/Models/People/DetailPeopleModel.swift
@@ -0,0 +1,16 @@
+//
+//  DetailPeopleModel.swift
+//  TMBD
+//
+//  Created by Dan FuPo on 19/01/24.
+//
+
+import Foundation
+
+struct DetailPeople: Codable, Identifiable {
+  var id: Int
+  var biography: String
+  var birthday: String
+  var deathday: String?
+  var place_of_birth: String
+}
